51976f05c497d5dda2d8db35c808477fb9d95c93
Eric de Ruiter <eric@thisisdevelopment.nl>
2020-04-01 10:15:47 +0200

    Added useragent to update.sh curl

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for release in 7.1 7.2 7.3 7.4; do
-    version=$(curl --retry 5 --retry-delay 10 -4 -vso- "https://www.php.net/releases/?json&version=${release}" 2>curl.log | tee curl.out | jq -r '.version');
+    version=$(curl --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36" --retry 5 --retry-delay 10 -4 -vso- "https://www.php.net/releases/?json&version=${release}" 2>curl.log | tee curl.out | jq -r '.version');
     if [ -z "${version}" ]; then
         cat curl.log
         cat curl.out
