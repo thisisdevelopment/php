ee2b3445589020c6edcb318a9151a4fbde5d51bf
Eric de Ruiter <eric@thisisdevelopment.nl>
2020-04-02 10:38:08 +0200

    workaround github limitation of only sending a webhook if you have <= 3 tags updated

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for release in 7.1 7.2 7.3 7.4; do
-    version=$(curl --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36" --retry 5 --retry-delay 10 -4 -vso- "https://www.php.net/releases/?json&version=${release}" 2>curl.log | tee curl.out | jq -r '.version');
+    version=$(curl --retry 5 --retry-delay 10 -4 -vso- "https://www.php.net/releases/?json&version=${release}" 2>curl.log | tee curl.out | jq -r '.version');
     if [ -z "${version}" ]; then
         cat curl.log
         cat curl.out
@@ -16,14 +16,16 @@ for release in 7.1 7.2 7.3 7.4; do
     if [ -z "${tag}" ]; then
         # create tag
         git tag "${version}"
+        git push origin "${version}:${version}"
         echo "added"
     elif [ -z "$(git tag -l "${version}" --points-at HEAD)" ]; then
         # update tag
         git tag -d "${version}" > /dev/null
+        git push --delete origin "${version}"
         git tag "${version}"
+        git push origin "${version}:${version}"
         echo "updated"
     else
         echo "already up-to-date"
     fi
 done
-git push --tags --force
